73b8a030de54c302e0607e241a7f48d14c3c0332
rb@pi3c <rb@pi3c>
2025-12-08 12:27:07 +0100

    tled1

--- Software/spidev/tled1.py
+++ Software/spidev/tled1.py
@@ -6,9 +6,9 @@ spi = spidev.SpiDev()
 
 spi.open(0,0)
 
-spi.max_speed_hz = 8000000
-spi.mode = 0
-spi.bits_per_word = 8
+spi.max_speed_hz = 1000000
+#spi.mode = 0
+#spi.bits_per_word = 8
 spi.lsbfirst = False
 
 class Color:
@@ -16,25 +16,16 @@ class Color:
         self.b = bytearray([r,g,b])
 
     def send(self):
-        spi.xfer(self.b)
+        print(f"Sending {self.b}")
+        spi.xfer2(self.b)
 
 blau = Color(0,0,0xFF)
-rot = Color(0xFF,0,0x0F)
-
-to_send = bytearray.fromhex("FF 00 00")
-print(to_send)
+rot = Color(0xFF,0,0x00)
 
 blau.send()
-#spi.xfer(to_send)
 
 time.sleep(1.0)
 
-to_send = b'\x00\xFF\x00'
-
-#spi.xfer2(to_send)
-
-
 rot.send()
-#spi.xfer2(to_send)
 
 spi.close()

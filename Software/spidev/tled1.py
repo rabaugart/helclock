b48a5db88ada37859a16dd018ea866d2907b07b5
rb@pi3c <rb@pi3c>
2025-12-08 21:23:51 +0100

    1 LED leuchtet korrekt

--- Software/spidev/tled1.py
+++ Software/spidev/tled1.py
@@ -6,8 +6,8 @@ spi = spidev.SpiDev()
 
 spi.open(0,0)
 
-#spi.max_speed_hz = 1000000
-#spi.mode = 0
+spi.max_speed_hz = 2000000
+#spi.mode = 1
 #spi.bits_per_word = 8
 #spi.lsbfirst = False
 
@@ -15,18 +15,20 @@ class Color:
     def __init__(self,r,g,b):
         self.b = bytearray([r,g,b])
 
-    def send(self):
+    def send(self,sleep=None):
         print(f"Sending {self.b}")
         spi.writebytes(self.b)
-        #spi.xfer(self.b,1000000, 50, 8)
+        #spi.xfer(self.b,1000000, 1)
+        if sleep:
+            time.sleep(sleep)
 
 blau = Color(0,0,0xFF)
-rot = Color(0x10,0,0x00)
+rot = Color(0xFF,0x00,0x00)
+grün = Color(0x00,0xFF,0X00)
 
-blau.send()
-
-time.sleep(1.0)
-
-rot.send()
+while True:
+    blau.send(1.0)
+    rot.send(1.0)
+    grün.send(1.0)
 
 spi.close()

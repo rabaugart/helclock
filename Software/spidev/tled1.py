07297e7e21fac7c262871bc58595d28194221011
rb@pi3c <rb@pi3c>
2025-12-08 13:53:24 +0100

    tled1

--- Software/spidev/tled1.py
+++ Software/spidev/tled1.py
@@ -9,7 +9,7 @@ spi.open(0,0)
 spi.max_speed_hz = 1000000
 #spi.mode = 0
 #spi.bits_per_word = 8
-spi.lsbfirst = False
+#spi.lsbfirst = False
 
 class Color:
     def __init__(self,r,g,b):
@@ -17,10 +17,11 @@ class Color:
 
     def send(self):
         print(f"Sending {self.b}")
-        spi.xfer2(self.b)
+        spi.writebytes(self.b)
+        #spi.xfer(self.b,1000000, 50, 8)
 
 blau = Color(0,0,0xFF)
-rot = Color(0xFF,0,0x00)
+rot = Color(0x10,0,0x00)
 
 blau.send()
 

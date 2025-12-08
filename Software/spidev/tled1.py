4d7d72c864a23fcfb78952133e252dcf1408feac
Netz <netz@Rabookmi>
2025-12-08 12:13:35 +0100

    tled1

--- Software/spidev/tled1.py
+++ Software/spidev/tled1.py
@@ -11,16 +11,27 @@ spi.mode = 0
 spi.bits_per_word = 8
 spi.lsbfirst = False
 
+class Color:
+    def __init__(self,r,g,b):
+        self.b = bytearray([r,g,b])
+
+    def send(self):
+        spi.xfer(self.b)
+
+blau = Color(0,0,0xFF)
+rot = Color(0xFF,0,0x0F)
+
 to_send = bytearray.fromhex("FF 00 00")
 print(to_send)
 
-spi.xfer(to_send)
+blau.send()
+#spi.xfer(to_send)
 
 time.sleep(1.0)
 
 to_send = b'\x00\xFF\x00'
 
+rot.send()
 #spi.xfer2(to_send)
 
 spi.close()
-

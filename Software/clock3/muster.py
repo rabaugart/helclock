f7439f68e3075138fa6682d865cd0192985a3d06
rb@pi3c <rb@pi3c>
2025-12-12 17:40:54 +0100

    Neues Muster

--- Software/clock3/muster.py
+++ Software/clock3/muster.py
@@ -5,15 +5,32 @@ import c3
 
 s = c3.Spi()
 
-l = 20
+SL = 15
 
-#s.putcolors([c3.BLAU]*l)
+#s.putcolors([c3.BLAU]*SL)
+
+def rotate(ba):
+    return [ba[-1]] + ba[:-1]
+
+def filled_color_array(cols,le,fillcolor=c3.SCHWARZ):
+    return cols + [fillcolor]*(le-len(cols))
 
 #sys.exit(0)
+
+ca = filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], SL )
+ca = ([c3.ROT,c3.GRÜN,c3.BLAU]+[c3.SCHWARZ]*2)*3
+ca = ([c3.GRÜN*0.3,c3.GRÜN,c3.GRÜN*0.3]+[c3.SCHWARZ]*2)*3
+
+s.putcolors([c3.SCHWARZ]*SL)
+
 while True:
-    s.putcolors([c3.ROT]*l)
+    s.putcolors(ca)
+    time.sleep(0.5)
+    ca = rotate(ca)
+    continue
+    s.putcolors([c3.ROT]*SL)
     time.sleep(1.0)
-    s.putcolors([c3.BLAU]*l)
+    s.putcolors([c3.BLAU]*SL)
     time.sleep(1.0)
-    s.putcolors([c3.GRÜN]*l)
+    s.putcolors([c3.GRÜN]*SL)
     time.sleep(1.0)

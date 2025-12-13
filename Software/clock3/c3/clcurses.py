cb2f023d99796b48512b00f688cf26ac9a5c2c5e
Netz <netz@Rabookmi>
2025-12-13 12:45:41 +0100

    Reduziere Update-Zyklen

--- Software/clock3/c3/clcurses.py
+++ Software/clock3/c3/clcurses.py
@@ -3,18 +3,24 @@ import curses, time
 from .hclock import index_buchstaben_pos, zeit_satz_indexe
 
 class CRenderer:
+    TIMEOUT = 100 # ms
+    UPDATE_ZYKLEN = 50
     def run(self):
         count = True
         w = curses.initscr()
-        w.timeout(100)
+        w.timeout(self.TIMEOUT)
         curses.savetty()
         w.clear()
         w.border()
         cw = 13
+        zyklus = 0
         while count:
             ch = w.getch()
             if ch >= 0:
                 cw = ch
+            zyklus = (zyklus+1) % self.UPDATE_ZYKLEN
+            if zyklus != 1:
+                continue
             w.clear()
             w.addstr(1,1,"Beenden mit q")
             for b,i,r,c in index_buchstaben_pos(zeit_satz_indexe()):

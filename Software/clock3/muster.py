40b5474ffb29b3f6cd4550d768a81b767ae79143
rb@pi3c <rb@pi3c>
2025-12-13 10:56:34 +0100

    Testmuster für Strang

--- Software/clock3/muster.py
+++ Software/clock3/muster.py
@@ -5,10 +5,10 @@ import c3, c3.gen
 
 s = c3.Spi()
 
-SL = 15
+SL = 110
 
-ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], SL )
-ca = ([c3.ROT,c3.GRÜN,c3.BLAU]+[c3.SCHWARZ]*2)*3
+ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], int(SL) )
+#ca = ([c3.ROT,c3.GRÜN,c3.BLAU]+[c3.SCHWARZ]*2)*3
 #ca = ([c3.GRÜN*0.3,c3.GRÜN,c3.GRÜN*0.3]+[c3.SCHWARZ]*2)*3
 
-s.putcolgen(c3.gen.blink(ca),0.3)
+s.putcolgen(c3.gen.rotate(ca),0.05)

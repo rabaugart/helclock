444c10b970c12beefdef7db448fad05b0fc5926f
rb@pi0a <rb@pi0a>
2025-12-14 23:03:18 +0100

    Schalte Uhr häufiger

--- Software/clock3/muster.py
+++ Software/clock3/muster.py
@@ -8,9 +8,9 @@ s = c3.Spi()
 SL = 110
 
 #ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], int(SL), c3.GRÜN )
-ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], int(SL/5), c3.WEISS )*5
+ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], int(SL/5), c3.BLAU )
 #ca = ([c3.ROT,c3.GRÜN,c3.BLAU]+[c3.SCHWARZ]*2)*3
 #ca = ([c3.GRÜN*0.3,c3.GRÜN,c3.GRÜN*0.3]+[c3.SCHWARZ]*2)*3
 
 #s.putcolgen(c3.gen.rotate(ca),0.1)
-s.putcolgen(c3.hclock.zeit_spi_generator(),5)
+s.putcolgen(c3.hclock.zeit_spi_generator(),0.1)

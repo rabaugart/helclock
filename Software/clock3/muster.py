ff8ab5067f50fe17069235a4681d2969c869886b
rb@pi0a <rb@pi0a>
2025-12-17 23:49:46 +0100

    Korrigiere Testmuster

--- Software/clock3/muster.py
+++ Software/clock3/muster.py
@@ -8,7 +8,7 @@ s = c3.Spi()
 SL = 110
 
 #ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], int(SL), c3.GRÜN )
-ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], int(SL/5), c3.BLAU )
+ca = c3.filled_color_array( [c3.ROT,c3.GRÜN,c3.BLAU], int(SL/5), c3.BLAU )*5
 #ca = ([c3.ROT,c3.GRÜN,c3.BLAU]+[c3.SCHWARZ]*2)*3
 #ca = ([c3.GRÜN*0.3,c3.GRÜN,c3.GRÜN*0.3]+[c3.SCHWARZ]*2)*3
 

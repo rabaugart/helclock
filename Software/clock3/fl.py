c2691051a7d1c6bb9d39444ba9f3423d9d9b49d2
Netz <netz@Rabookmi>
2025-12-19 22:58:10 +0100

    Websocket auf 0.0.0.0

--- Software/clock3/fl.py
+++ Software/clock3/fl.py
@@ -90,7 +90,7 @@ async def echo(websocket):
         #await websocket.send(message)
 
 async def wsmain():
-    async with serve(echo, "localhost", 8765) as server:
+    async with serve(echo, "0.0.0.0", 8765) as server:
         await server.serve_forever()
 
 if __name__ == "__main__":

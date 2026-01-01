06961e7c6813626e302f046681938577fd3ba5fe
Netz <netz@Rabookmi>
2026-01-01 12:31:16 +0100

    Korrigiere TestGen2

--- Software/clock3/c3/server/agenerator.py
+++ Software/clock3/c3/server/agenerator.py
@@ -52,11 +52,12 @@ class TestGen2:
             self.farbmap[COL_SECT.Mittelfarbe]]+
             [self.farbmap[COL_SECT.Hintergrund]]*(self.slen-2))*self.pnum
         print(f"Starte TestGen2 mit Länge {len(l)}")
-        for i in l:
+        for i in [self.farbmap[COL_SECT.Vordergrund],
+            self.farbmap[COL_SECT.Mittelfarbe],self.farbmap[COL_SECT.Hintergrund]]:
             yield colors_bytes([i]*self.slen),0.1 if count < 1 else 5.0
             count += 1
         while True:
-            yield colors_bytes(l),0.5
+            yield colors_bytes(l),0.3
             l = rotate_list(l)
 
 class AGenerator(dict):
